5253a73082a332be5457502bf766bf9d533b3d6e
alcantaara <mari.alcantaara@gmail.com>
2017-07-21 21:32:05 -0300

    Versao 1.0

---
+++ A.java
@@ -0,0 +1,25 @@
+package teste;
+ //criando a classe
+public class A {
+	private int a1; //atributos
+	private float a2; 
+
+	
+	    public static void main(String[] args) {
+	      
+	    }
+	    public int getA1() { 
+	        return a1;
+	    }
+
+	    public void setA1(int a1) {
+	        this.a1 = a1;
+	    }
+	    public float getA2() {
+	        return a2;
+	    }
+
+	    public void setA2(float a2) {
+	        this.a2 = a2;
+	    }
+}
